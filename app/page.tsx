752f37ddc58623521b8d5475095ef968be904139
Krystian <texen24@gmail.com>
2025-03-02 16:39:51 +0100

    install shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 const HomePage = () => {
   return (
-    <h1 className="text-3xl">HomePage</h1>
-  )
-}
-export default HomePage
+    <div>
+      <h1 className='text-3xl'>HomePage</h1>
+      <Button variant='outline' size='lg' className='capitalize m-8'>
+        Click
+      </Button>
+    </div>
+  );
+};
+export default HomePage;
